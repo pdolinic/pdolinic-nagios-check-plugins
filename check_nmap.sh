97fd73d5cff3ea80f35ea1688f5fea77b4d4f23f
pdolinic <71389319+pdolinic@users.noreply.github.com>
2022-04-11 09:54:33 +0200

    Add files via upload
    Usage: arg0=Programname  arg1=Hostname arg2=Port arg3=ClosedExitCode arg4=OpenedExitCode arg5=FilteredExitCode"
    ./check_nmap.sh example.com 22 1 0 2"

---
+++ check_nmap.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+## Description: Small Check for Educational Purposes 
+## Author: pdolinic@netways.de, Junior Consultant at NETWAYS Professional Services GmbH. Deutschherrnstr. 15-19 90429 Nuremberg. 
+
+# Check Single Ports
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+##This Script is not intended with security in mind, and currently does not implement any length or size checks: It depends on the security of the Icinga/Nagios-Account.
+
+if [ $# == 0 ]
+then
+    echo "Usage: arg0=Programname  arg1=Hostname arg2=Port arg3=ClosedExitCode arg4=OpenedExitCode arg5=FilteredExitCode"
+    echo "./check_nmap.sh example.com 22 1 0 2"
+fi
+
+host="$1"
+port="$2"
+ce="$3"
+oe="$4"
+fe="$5"
+
+clsd="closed"
+opn="open"
+fltrd="filtered"
+
+openedcheck=$(/usr/bin/sudo /usr/bin/nmap -p $port $host | grep "open"| awk '{ print $2 }' ) 
+filteredcheck=$(/usr/bin/sudo /usr/bin/nmap  -p $port $host | grep "filtered"| awk '{ print $2 }' ) 
+closedcheck=$(/usr/bin/sudo /usr/bin/nmap -p $port $host | grep "closed"| awk '{ print $2 }' )
+
+o_res=$(/usr/bin/sudo /usr/bin/nmap -p $port $host | grep "open" | awk '{ print $1,$2 }' )
+f_res=$(/usr/bin/sudo /usr/bin/nmap -p $port $host | grep "filtered" | awk '{ print $1,$2 }' ) 
+c_res=$(/usr/bin/sudo /usr/bin/nmap -p $port $host | grep "closed" | awk '{ print $1,$2 }' )
+
+
+if [ "$closedcheck" == $clsd ]
+then
+    echo $c_res
+    exit $ce
+fi
+
+if [ "$openedcheck" == $opn ]
+then
+    echo $o_res
+    exit $oe
+fi
+
+if [ "$filteredcheck" == $fltrd ]
+then
+    echo $f_res   
+    exit $fe
+fi
