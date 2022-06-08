c683d63e06f9f73e5e8410b587c6849b84525e60
pdolinic <71389319+pdolinic@users.noreply.github.com>
2022-06-09 01:57:03 +0200

    Add ASCII-Art and Comments

--- check_nmap.sh
+++ check_nmap.sh
@@ -5,6 +5,8 @@
 
 # Check Single Ports
 
+#License = GNU GPLv2
+
 #This program is distributed in the hope that it will be useful,
 #but WITHOUT ANY WARRANTY; without even the implied warranty of
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -12,18 +14,34 @@
 
 ##This Script is not intended with security in mind, and currently does not implement any length or size checks: It depends on the security of the Icinga/Nagios-Account.
 
-if [ $# == 0 ]
+#   ______ _     _ _______ ______ _    _         ______  ______         ______   
+#   / _____) |   | (_______) _____) |  / )       |  ___ \|  ___ \   /\  (_____ \ 
+#  | /     | |__ | |_____ | /     | | / /        | |   | | | _ | | /  \  _____) )  
+#  | |     |  __)| |  ___)| |     | |< <         | |   | | || || |/ /\ \|  ____/ 
+#  | \_____| |   | | |____| \_____| | \ \ _______| |   | | || || | |__| | |
+#   \______)_|   |_|_______)______)_|  \_|_______)_|   |_|_||_||_|______|_|
+#
+
+#Exit Codes
+Exit_Codes=" >> 0:OK, 1:Warn, 2:Critical, 3:Unkown << "
+
+if [ -z "$1" ] || [ -z "$2" ] 
 then
-    echo "Usage: arg0=Programname  arg1=Hostname arg2=Port arg3=ClosedExitCode arg4=OpenedExitCode arg5=FilteredExitCode"
-    echo "./check_nmap.sh example.com 22 1 0 2"
+    echo ">> Usage: arg0=programname arg1=hostname arg2=port arg3=Your_Exit_Code#1 arg4=Your_Exit_Code#2 arg5=Your_Exit_Code#3 << " && \
+    echo ">> Example: ./check_nmap.sh example.com 445 1 2 0 <<" && \
+    echo $Exit_Codes
 fi
 
+#Generate Temporaty Nmap-Savefile
+RandomID=$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 20; echo;)
 now=$(date +"%Y-%m-%d-%h-%s")
-filename="/tmp/nmap-save-${now}"
+filename="/tmp/nmap-save-${RandomID}"
 
+#Add Arguments for Host & Port
 host="$1"
 port="$2"
 
+#Exit Codes you as an Admin can adapt for your Needs
 cec="$3"
 oec="$4"
 fec="$5"
@@ -32,11 +50,15 @@ clsd="closed"
 opn="open"
 fltrd="filtered"
 
+#Run Scan
 Nmap_Save=$(/usr/bin/nmap -p $port $host -oN $filename)
+
+#Check States
 OpenedCheck=$(grep "open" $filename | awk '{ print $2 }' ) 
 FilteredCheck=$(grep "filtered" $filename | awk '{ print $2 }' ) 
 ClosedCheck=$(grep "closed" $filename | awk '{ print $2 }' )
 
+#Return Resuls
 Opened_Result=$(grep "open" $filename | awk '{ print $1,$2 }' )
 Filtered_Result=$(grep "filtered" $filename | awk '{ print $1,$2 }' ) 
 Closed_Result=$(grep "closed" $filename | awk '{ print $1,$2 }' )
